aa34747366b43732db8740a364e0b15467be27e4
Andres Galvez <pocyxDesigner@hotmail.com>
2016-10-17 09:46:50 +0200

    Fuera de gravedad

---
+++ Ejer6Tema4.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjerciciosProgramacion4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author PocyxDesigner
+ */
+public class Ejer6Tema4 {
+    public static void main(String[] args) {
+   
+        Scanner s = new Scanner(System.in);
+        System.out.print("      -TIEMPO QUE TARDA EN CAER UN OBJETO- \n\n");
+        System.out.print("Introduce la altura en metros: "); 
+        double h = Double.parseDouble(s.nextLine()); 
+        double g = 9.81;
+   
+        double tiempo = Math.sqrt((2*h) / g);
+        
+       
+        
+        
+        System.out.println("\nSi la altura en la que esta el objeo es "+h+
+                           " metros, el tiempo que tardará en caer será: "+tiempo+" segundos");
+        
+    }
+}
