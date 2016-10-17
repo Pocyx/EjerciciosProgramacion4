9a7d46fa6a864f54e0639e7202e0331624373454
Andres Galvez <pocyxDesigner@hotmail.com>
2016-10-17 10:11:30 +0200

    Media de tres notas

---
+++ Ejer7Tema4.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjerciciosProgramacion4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author PocyxDesigner
+ */
+public class Ejer7Tema4 {
+    public static void main(String[] args) {
+   
+        Scanner s = new Scanner(System.in);
+        System.out.print("      -CALCULA LA MEDIA- \n\n");
+        System.out.print("Introduce la primera nota: "); 
+        double nota1 = Double.parseDouble(s.nextLine()); 
+        System.out.print("Introduce la segunda nota: "); 
+        double nota2 = Double.parseDouble(s.nextLine());
+        System.out.print("Introduce la tercera nota: "); 
+        double nota3 = Integer.parseInt(s.nextLine());
+        double media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println("La media es : " + media);
+        
+    }
+}
