7839bdb78649432a0b2c326adcbf7d536d31003d
Andrés Manuel <pocyxdesigner@hotmail.com>
2016-10-21 12:22:00 +0200

    Update Ejer10Tema4.java

--- Ejer10Tema4.java
+++ Ejer10Tema4.java
@@ -23,15 +23,15 @@ public class Ejer10Tema4 {
         int mes = Integer.parseInt(s.nextLine());
         
         if((dia>=21 && mes==3) || (dia<=20 && mes==4 )){
-            System.out.print("Aries ");
+            System.out.print("No te lavas las cAries ");
         }else if((dia>=21 && mes==4) || (dia<=20 && mes==5 )){
             System.out.print("Taurus ");
         }else if((dia>=21 && mes==5) || (dia<=22 && mes==6 )){
             System.out.print("Géminis ");
         }else if((dia>=23 && mes==6) || (dia<=22 && mes==7 )){
-            System.out.print("Cancer ");
+            System.out.print("Tu curaras el Cancer ");
         }else if((dia>=23 && mes==7) || (dia<=23 && mes==8 )){
-            System.out.print("Leo ");
+            System.out.print("Leo mucho ");
         }else if((dia>=24 && mes==8) || (dia<=23 && mes==9 )){
             System.out.print("Virgo ");
         }else if((dia>=24 && mes==9) || (dia<=23 && mes==10 )){
@@ -39,11 +39,11 @@ public class Ejer10Tema4 {
         }else if((dia>=24 && mes==10) || (dia<=22 && mes==11 )){
             System.out.print("Scropio ");
         }else if((dia>=23 && mes==11) || (dia<=21 && mes==12 )){
-            System.out.print("Sagitario ");
+            System.out.print("Se agita a diario ");
         }else if((dia>=22 && mes==12) || (dia<=20 && mes==1 )){
-            System.out.print("Capricornio ");
+            System.out.print("Eres el Capricho de un unicornio ");
         }else if((dia>=21 && mes==1) || (dia<=20 && mes==2 )){
-            System.out.print("Acuario ");
+            System.out.print("Acuario fresquito! ");
         }else if((dia>=21 && mes==2) || (dia<=20 && mes==3 )){
             System.out.print("Piscis ");
         }else{
